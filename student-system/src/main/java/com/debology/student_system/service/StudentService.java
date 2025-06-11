4ede57e90facef1fd6f7dd6d45e52e615d67426d
Debesh P. <139678494+debeshp6@users.noreply.github.com>
2025-06-12 00:23:26 +0530

    backend

---
+++ student-system/src/main/java/com/debology/student_system/service/StudentService.java
@@ -0,0 +1,11 @@
+package com.debology.student_system.service;
+
+import com.debology.student_system.model.Student;
+
+import java.util.List;
+
+public interface StudentService {
+
+    public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
+}
